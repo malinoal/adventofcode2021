e053c66c6ccceb691625da009f1cfb13636ae1f9
malinoal <malinoal@campus.tu-berlin.de>
2021-12-12 14:43:46 +0100

    Day Four part two

--- 4/four.java
+++ 4/four.java
@@ -1,9 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -73,6 +70,54 @@ public class four {
     }
 
     public static void part_two() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("4/input.txt"));
+        String firstLine = input.nextLine();
+
+        List<Integer> drawnNumbers = Arrays.stream(firstLine.split(","))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        List<Integer> bingo = new ArrayList<>();
+
+        while(input.hasNext()){
+            bingo.add(input.nextInt());
+        }
+
+        List<Integer> hitPositions = new ArrayList<>();
+        Set<Integer> wonBoards = new LinkedHashSet<>();
+
+        for(int number : drawnNumbers) {
+            for (int i = 0; i < bingo.size(); i++) {
+                if (bingo.get(i) == number) {
+                    hitPositions.add(i);
+                }
+            }
+            hitPositions = hitPositions.stream().sorted().collect(Collectors.toList());
+            for (int position : hitPositions) {
+                int rowstart = position - (position % 5);
+                int[] row = IntStream.range(rowstart, rowstart + 5).toArray();
+                int columnPos = position % 5;
+                int bingoField = position % 25;
+                int columnStart = position - bingoField + columnPos;
+                int[] column = {columnStart, columnStart + 5, columnStart + 10, columnStart + 15, columnStart + 20};
+                boolean rowBingo = isBingo(hitPositions, row);
+                boolean columnBingo = isBingo(hitPositions, column);
+
+                if(rowBingo || columnBingo) {
+                    wonBoards.add(position / 25);
+                    if (wonBoards.size() == (bingo.size() / 25)) {
+                        int score = 0;
+                        for (int i = position - bingoField; i < position - bingoField + 25; i++) {
+                            if (!hitPositions.contains(i)) {
+                                score += bingo.get(i);
+                            }
+                        }
+                        score *= number;
+                        System.out.println(score);
+                        return;
+                    }
+                }
+            }
+        }
 
     }
 }
